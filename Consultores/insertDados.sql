19961f9349a2ac5f2668c07807a3700a37c4c813
tgiovanella <phptgmg@gmail.com>
2019-09-17 20:12:21 -0300

    Insert

--- Consultores/insertDados.sql
+++ Consultores/insertDados.sql
@@ -1,5 +1,16 @@
+## É possível fazer mais de um insert de uma única vez da forma demonstrada abaixo
 insert into consultores(nome, email) values
 ('Thiago Giovanella','thiago@giovanella.com'),
 ('Marco Antonio','marco@antonio.com'),
 ('Maria Lucia','marialucia@consultoria.com'),
 ('Pedro Honório','pedro@honorioconsultoria.com');
+
+insert into clientes(descricao, responsavel, email, telefone) values
+('Armazem de Cascalho','Tadeu Rezende', 'armazem@armazem.com.br','3532323211'),
+('Jurandir Caça e Pesca','Jurandir Mioranza', 'cacapesca@hotmail.com','3536545452'),
+('Joca Artes','Joaquim Carneiro Filho', 'filhocarneiro@joca.com.br','353645152');
+
+insert into especialidades(descricao) values
+('Consultoria em Bancos de Dados'),
+('Consultoria em Segurança da Informação'),
+('Gestão de Projetos');
